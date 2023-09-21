60803f81b7698937c6546f5cf0f8d0ed63cfccc3
HarrisWN <144472595+HarrisWN@users.noreply.github.com>
2023-09-20 20:40:34 -0400

    Add files via upload

---
+++ Week2/python_1_assignment_3_module.py
@@ -0,0 +1,18 @@
+### **Task 2: Create a Module and Import it**
+#Create a Module in VS Code and import it into a Notebook. Use each of the functions you build in your module
+
+#Your module should have the following capabilities:**
+#Has a function to calculate the square footage of a house
+#Has a function to calculate the circumference of a circle
+#Has a function to change feet to inches
+
+
+def square_footage_calculator (length,width):
+    return length * width
+
+def calculate_circumference(radius):
+    return(2 * 3.14 *radius)
+
+
+def distance_conversion(feet):
+    return(feet * 12)
